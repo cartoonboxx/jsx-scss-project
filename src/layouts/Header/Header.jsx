9550c1635dbae0135d21c7b2603ea3c254803b47
cartoonboxx <shulenin_2005@mail.ru>
2025-02-20 00:53:54 +0300

    lesson 2.2

---
+++ src/layouts/Header/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.scss'
+import Logo from "@/components/Logo";
+
+const Header = (props) => {
+    return (
+        <header className="header">
+            <div className="header__inner container">
+                <Logo
+                    className="header__logo"
+                    loading="eager"
+                />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
